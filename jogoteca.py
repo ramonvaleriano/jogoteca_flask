2a53bbd9c9c36a480c296b1df39213b73ed4be73
ramonvaleriano <rrvaleriano@gmail.com>
2021-02-16 14:01:45 -0300

    Criando depêndencia para criar jogo
    Só é possível criar um jogo se estiver logado.

--- jogoteca.py
+++ jogoteca.py
@@ -24,7 +24,9 @@ def index():
 
 @app.route('/novo')
 def novo():
-     return render_template('novo.html', titulo='Novo Jogo')
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect('/login')
+    return render_template('novo.html', titulo='Novo Jogo')
 
 
 @app.route('/criar', methods=['POST',])
